1568b5ee1fbc1ae046e33ed32440f3ab477a02a3
Fishcount56 <l.fadhilah80@gmail.com>
2022-04-17 21:28:15 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const API = axios.create({
-  baseURL: "https://lf-wow-server.herokuapp.com/api/v1/" || process.env.REACT_APP_SERVER_URL
+  baseURL: process.env.REACT_APP_SERVER_URL || "https://lf-wow-server.herokuapp.com/api/v1/" || "http://localhost:5000/api/v1/"
 })
 
 export const setAuthToken = (token) => {
